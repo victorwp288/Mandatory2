58354b5aae55d17d60b4d94b6e1a87c5d8f27dbf
victor petersen <victorwpetersen@gmail.com>
2022-02-10 11:03:37 +0100

    Added dynamic routing

--- tcpserver.py
+++ tcpserver.py
@@ -20,19 +20,20 @@ server_socket.listen(1)
 print("The server is ready to receive")
 while True:
     conn_socket,client_address = server_socket.accept()
-    try:
-        modified_message = conn_socket.recv(2048).decode().upper()
-        #conn_socket.send(modified_message.encode())
-        print("incoming ip:",client_address)
-        print("connection received from {}, and {} is sent back".format(client_address[1],modified_message))
+    modified_message = conn_socket.recv(2048).decode().upper()
+    #conn_socket.send(modified_message.encode())
+    print("incoming ip:",client_address)
+    print("connection received from {}, and {} is sent back".format(client_address[1],modified_message))
+    print(modified_message.split(" ")[1])
+    if modified_message.split(" ")[1] == "/":
         conn_socket.send(('HTTP/1.0 200 OK\r\n\r\n').encode())
         conn_socket.send(send_back_index().encode())
-    except:
-        conn_socket.send(('404 Not Found').encode())
-        #Close client socket
-        conn_socket.close()
-        print("connection closed")
-
+    elif modified_message.split(" ")[1] == "/TEST":
+        conn_socket.send(('HTTP/1.0 200 OK\r\n\r\n').encode())
+        conn_socket.send(send_back_test().encode())
+    else:
+        conn_socket.send(('HTTP/1.0 404 Not Found\r\n\r\n').encode())
+        conn_socket.send(('<html><body><h1>404 Not Found</h1></body></html>').encode())
 
     conn_socket.close()
 server_socket.close()
