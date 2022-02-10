26161a02db1dd2973ad1d25ef987e70e6c1f23f8
victor petersen <victorwpetersen@gmail.com>
2022-02-10 10:47:09 +0100

    added try / except

--- tcpserver.py
+++ tcpserver.py
@@ -2,8 +2,13 @@
 
 from socket import *
 
-def send_back():
+def send_back_index():
     f = open("index.html", "r")
+    html = f.read()
+    return html
+
+def send_back_test():
+    f = open("test.html", "r")
     html = f.read()
     return html
 
@@ -15,10 +20,19 @@ server_socket.listen(1)
 print("The server is ready to receive")
 while True:
     conn_socket,client_address = server_socket.accept()
-    modified_message = conn_socket.recv(2048).decode().upper()
-    #conn_socket.send(modified_message.encode())
-    print("incoming ip:",client_address)
-    print("connection received from {}, and {} is sent back".format(client_address[1],modified_message))
-    conn_socket.send(send_back().encode())
+    try:
+        modified_message = conn_socket.recv(2048).decode().upper()
+        #conn_socket.send(modified_message.encode())
+        print("incoming ip:",client_address)
+        print("connection received from {}, and {} is sent back".format(client_address[1],modified_message))
+        conn_socket.send(('HTTP/1.0 200 OK\r\n\r\n').encode())
+        conn_socket.send(send_back_index().encode())
+    except:
+        conn_socket.send(('404 Not Found').encode())
+        #Close client socket
+        conn_socket.close()
+        print("connection closed")
+
+
     conn_socket.close()
 server_socket.close()
