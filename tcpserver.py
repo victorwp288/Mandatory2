ae368dc8b6220eff5af330343ee6762fd4f2a38b
victor petersen <victorwpetersen@gmail.com>
2022-02-10 11:33:53 +0100

    Started to add loggin

--- tcpserver.py
+++ tcpserver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from socket import *
+from datetime import *
 
 def send_back_index():
     f = open("index.html", "r")
@@ -11,6 +12,22 @@ def send_back_test():
     f = open("test.html", "r")
     html = f.read()
     return html
+
+#ikke færdig
+def logging(response_code):
+    f = open("logfile.log", "a")
+    ip_from_client = client_address[1]
+
+    #man får HOST: med, dette er ikke godt!!!
+    response = modified_message.split(" ")[0:3]
+    print(response)
+
+    #Ligner ikke ip-adresse
+    f.write(str(ip_from_client))
+    f.write(str(datetime.now()))
+    f.write(str(response))
+    f.write(response_code)
+
 
 server_port = 80
 server_socket = socket(AF_INET,SOCK_STREAM)
@@ -25,15 +42,20 @@ while True:
     print("incoming ip:",client_address)
     print("connection received from {}, and {} is sent back".format(client_address[1],modified_message))
     print(modified_message.split(" ")[1])
+    response_200 = "HTTP/1.1 200 OK\r\n\r\n"
+    response_404 = "HTTP/1.1 404 Not Found\r\n\r\n"
     if modified_message.split(" ")[1] == "/":
-        conn_socket.send(('HTTP/1.0 200 OK\r\n\r\n').encode())
+        conn_socket.send((response_200).encode())
         conn_socket.send(send_back_index().encode())
+        logging(response_200)
     elif modified_message.split(" ")[1] == "/TEST":
-        conn_socket.send(('HTTP/1.0 200 OK\r\n\r\n').encode())
+        conn_socket.send((response_200).encode())
         conn_socket.send(send_back_test().encode())
+        logging(response_200)
     else:
-        conn_socket.send(('HTTP/1.0 404 Not Found\r\n\r\n').encode())
+        conn_socket.send((response_404).encode())
         conn_socket.send(('<html><body><h1>404 Not Found</h1></body></html>').encode())
+        logging(response_404)
 
     conn_socket.close()
 server_socket.close()
